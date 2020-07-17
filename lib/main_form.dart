8ecdb3bf7b8f43172209bbf850adf5f1bba2d43b
Masayuki Ono <mono0926@gmail.com>
2020-07-17 10:46:13 +0900

    他に問題の起こる例と、正しい調整方法を3通り追加
    - Columnのchildrenに足していったら高さが足りなくなってoverflowエラーが起こったのでListViewに変更もした
      - 小さい端末や横にしたとき・キーボード表示時など高さはすぐ足りなくなるのでページで何か縦に置いていくときはListView系を常用するのがお勧め

--- lib/main_form.dart
+++ lib/main_form.dart
@@ -19,18 +19,24 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Form(
-          child: Column(
-            children: const [
-              _NormalTextForm(),
-              SizedBox(height: 24),
-              _SizedBoxTextForm1(),
-              SizedBox(height: 24),
-              _SizedBoxTextForm2(),
-            ],
-          ),
+      body: Form(
+        child: ListView(
+          padding: const EdgeInsets.all(16),
+          children: const [
+            _NormalTextForm(),
+            SizedBox(height: 24),
+            _SizedBoxTextForm1(),
+            SizedBox(height: 24),
+            _SizedBoxTextForm2(),
+            SizedBox(height: 24),
+            _SizedBoxTextForm3(),
+            SizedBox(height: 24),
+            _DenseTextForm(),
+            SizedBox(height: 24),
+            _DenseTextForm2(),
+            SizedBox(height: 24),
+            _DenseTextForm3(),
+          ],
         ),
       ),
     );
@@ -81,6 +87,89 @@ class _SizedBoxTextForm2 extends StatelessWidget {
 
           /// heightを指定すると、helperText込みで44になってしまう
           /// フォーム部分だけ高さ44で、helpertextやerrorTextはその下に配置したい
+          helperText: 'これはヘルパーテキストです',
+        ),
+      ),
+    );
+  }
+}
+
+class _SizedBoxTextForm3 extends StatelessWidget {
+  const _SizedBoxTextForm3({Key key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      /// TextFormFieldを無理やりSizedBoxで高さを指定するとカーソルの位置が中心に合わない
+      height: 44,
+      child: TextFormField(
+        decoration: const InputDecoration(
+          border: OutlineInputBorder(),
+        ),
+        // heightを指定すると、fontSizeが大きくなった時に崩れる
+        // (OS設定でtextScaleFactorによって自動的に大きくなったときなども同様)
+        style: const TextStyle(fontSize: 40),
+      ),
+    );
+  }
+}
+
+class _DenseTextForm extends StatelessWidget {
+  const _DenseTextForm({Key key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8),
+        ),
+        labelText: 'これはラベルです',
+        helperText: 'これはヘルパーテキストです',
+        // カスタマイズ性は少ないが簡単
+        isDense: true,
+      ),
+    );
+  }
+}
+
+class _DenseTextForm2 extends StatelessWidget {
+  const _DenseTextForm2({Key key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8),
+        ),
+        labelText: 'これはラベルです',
+        helperText: 'これはヘルパーテキストです',
+        contentPadding: const EdgeInsets.symmetric(
+          // ここで調整
+          vertical: 0,
+          horizontal: 12,
+        ),
+      ),
+    );
+  }
+}
+
+class _DenseTextForm3 extends StatelessWidget {
+  const _DenseTextForm3({Key key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Theme(
+      data: Theme.of(context).copyWith(
+        // ここで調整
+        // 個別に詰め気味にしたいならこのように特定Widgetだけ囲むが、
+        // アプリ全体をこれで囲んで全体的に詰まり気味にもできる
+        // (標準Widgetは大抵これに対応しているのでお行儀よく使っていると追従してくれる)
+        visualDensity: VisualDensity.compact,
+      ),
+      child: TextFormField(
+        decoration: InputDecoration(
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8),
+          ),
+          labelText: 'これはラベルです',
           helperText: 'これはヘルパーテキストです',
         ),
       ),
