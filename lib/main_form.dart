926ed7d1e74a44cc99814d36dd41ce3648328bd3
HTsuruo <tsuru.dev@gmail.com>
2020-07-17 11:39:33 +0900

    isDenseやcontentPaddingを使ってTextFormFieldの高さをカスタマイズする
    -
    Sizedboxで包んでheight指定してしまうとフォントサイズなどの影響を受けて途端にデザイン崩れが発生してしまう

--- lib/main_form.dart
+++ lib/main_form.dart
@@ -22,13 +22,27 @@ class HomePage extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(16),
         child: Form(
-          child: Column(
+          child: ListView(
             children: const [
+              Text('NGなやりかた'),
+              const Divider(),
               _NormalTextForm(),
               SizedBox(height: 24),
               _SizedBoxTextForm1(),
               SizedBox(height: 24),
               _SizedBoxTextForm2(),
+              SizedBox(height: 24),
+              _SizedBoxTextForm3(),
+              SizedBox(height: 24),
+              Text('GOODなやりかた'),
+              const Divider(),
+              _DenseTextForm(),
+              SizedBox(height: 24),
+              _DenseTextForm2(),
+              SizedBox(height: 24),
+              _DenseTextForm3(),
+              SizedBox(height: 24),
+              SizedBox(height: 24),
             ],
           ),
         ),
@@ -81,6 +95,100 @@ class _SizedBoxTextForm2 extends StatelessWidget {
 
           /// heightを指定すると、helperText込みで44になってしまう
           /// フォーム部分だけ高さ44で、helpertextやerrorTextはその下に配置したい
+          helperText: 'これはヘルパーテキストです',
+        ),
+      ),
+    );
+  }
+}
+
+class _SizedBoxTextForm3 extends StatelessWidget {
+  const _SizedBoxTextForm3({Key key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 44,
+      child: TextFormField(
+        decoration: const InputDecoration(
+          border: OutlineInputBorder(),
+        ),
+
+        /// フォントサイズを大きくしたりOS設定で変更になると
+        /// height指定はそれに適応できない
+        style: TextStyle(fontSize: 44),
+      ),
+    );
+  }
+}
+
+/// isDenseを使ったパターン：
+/// 「uses less vertical space」 高さが気になる場合にサクッと修正できる
+class _DenseTextForm extends StatelessWidget {
+  const _DenseTextForm({Key key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8),
+        ),
+        labelText: 'これはラベルです',
+        helperText: 'これはヘルパーテキストです',
+        // カスタマイズ性は少ないが簡単
+        isDense: true,
+      ),
+    );
+  }
+}
+
+/// contentPaddingを使ったパターン
+/// isDenseより調整がきく
+/// If `isOutline` property of [border] is true then `contentPaddding` is
+/// `EdgeInsets.fromLTRB(12, 20, 12, 12)` when [isDense] is true
+/// and `EdgeInsets.fromLTRB(12, 24, 12, 16)` when [isDense] is false.
+/// 上記のisDenseを指定するパターンだと内部的にcontentPaddingを(12, 20, 12, 12)に指定している
+class _DenseTextForm2 extends StatelessWidget {
+  const _DenseTextForm2({Key key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8),
+        ),
+        labelText: 'これはラベルです',
+        helperText: 'これはヘルパーテキストです',
+        contentPadding: const EdgeInsets.symmetric(
+          // ここで調整
+          vertical: 0,
+          horizontal: 12,
+        ),
+      ),
+    );
+  }
+}
+
+/// Themeで指定するパターン
+/// フォームの高さを全て共通にする場合（ベースを用意する場合）はこれが良さそう
+/// ただしフォーム以外も全体的にcompactになるのはやや懸念か
+class _DenseTextForm3 extends StatelessWidget {
+  const _DenseTextForm3({Key key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Theme(
+      data: Theme.of(context).copyWith(
+        // ここで調整
+        // 個別に詰め気味にしたいならこのように特定Widgetだけ囲むが、
+        // アプリ全体をこれで囲んで全体的に詰まり気味にもできる
+        // (標準Widgetは大抵これに対応しているのでお行儀よく使っていると追従してくれる)
+        visualDensity: VisualDensity.compact,
+      ),
+      child: TextFormField(
+        decoration: InputDecoration(
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8),
+          ),
+          labelText: 'これはラベルです',
           helperText: 'これはヘルパーテキストです',
         ),
       ),
