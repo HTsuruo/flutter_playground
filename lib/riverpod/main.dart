a0b55423428f00815276b34f030e5c3217579005
HTsuruo <tsuru.dev@gmail.com>
2020-08-18 12:14:19 +0900

    riverpodをゴニョゴニョ触ってみる`

---
+++ lib/riverpod/main.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_playground/riverpod/state_notifier.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+// 通常のProvider（値を渡すだけ）
+final sampleProvider = Provider((ref) => 'tsuruoka4');
+final sampleStateProvider = StateProvider((ref) => 'tsuruokaState');
+
+void main() => runApp(
+      const ProviderScope(
+        child: App(),
+      ),
+    );
+
+class App extends StatelessWidget {
+  const App({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: HomePage.routeName,
+      routes: {
+        HomePage.routeName: (context) => const HomePage(),
+        StateNotifierPage.routeName: (context) => const StateNotifierPage(),
+      },
+    );
+  }
+}
+
+class HomePage extends HookWidget {
+  const HomePage({Key key}) : super(key: key);
+
+  static const routeName = '/';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(),
+        body: ListView(
+          children: [
+            ListTile(
+              title: const Text('StateNotifier'),
+              trailing: const Icon(Icons.navigate_next),
+              onTap: () =>
+                  Navigator.pushNamed(context, StateNotifierPage.routeName),
+            )
+          ],
+        ));
+  }
+}
+
+//
+//class _Text extends HookWidget {
+//  const _Text({
+//    Key key,
+//  }) : super(key: key);
+//
+//  @override
+//  Widget build(BuildContext context) {
+//    return Text(useProvider(sampleProvider));
+//  }
+//}
+//
+//class _StateNotifierText extends HookWidget {
+//  const _StateNotifierText({Key key}) : super(key: key);
+//  @override
+//  Widget build(BuildContext context) {
+//    return Text(useProvider(sampleStateNotifierProvider.state).toString());
+//  }
+//}
+//
+//// stateless widgetだけでなく、statefulのwidgetも用意されている
+//class _StateFulSample extends StatefulHookWidget {
+//  const _StateFulSample({Key key}) : super(key: key);
+//  @override
+//  _StateFulSampleState createState() => _StateFulSampleState();
+//}
+//
+//class _StateFulSampleState extends State<_StateFulSample> {
+//  @override
+//  Widget build(BuildContext context) {
+//    return Text('this is stateful : ${useProvider(sampleProvider)}');
+//  }
+//}
---
+++ lib/riverpod/state_notifier.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:state_notifier/state_notifier.dart';
+
+final sampleStateNotifierProvider = StateNotifierProvider.autoDispose(
+  (ref) {
+    final controller = SampleController();
+    return controller;
+  },
+);
+
+class SampleController extends StateNotifier<int> {
+  SampleController() : super(0) {
+    _init();
+  }
+  void increment() {
+    print('increment');
+    state++;
+  }
+
+  void _init() {
+    print('init');
+  }
+
+  @override
+  void dispose() {
+    print('dispose');
+    super.dispose();
+  }
+}
+
+class StateNotifierPage extends HookWidget {
+  const StateNotifierPage({Key key}) : super(key: key);
+
+  static const routeName = '/state_notifier';
+
+  @override
+  Widget build(BuildContext context) {
+//    final state = useProvider(sampleStateNotifierProvider.state);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('StateNotifier'),
+      ),
+      body: const SafeArea(
+        child: Center(
+          child: Text('state notifier sample'),
+        ),
+      ),
+      floatingActionButton: const _FabUserProviderPattern(),
+//      floatingActionButton: const _FabContextPattern(),
+    );
+  }
+}
+
+// buildの中でuseProviderを使いcontrollerを宣言した場合
+// 画面遷移時にinit, ボタン押下時にincrement, 画面を閉じたらdispose
+class _FabUserProviderPattern extends HookWidget {
+  const _FabUserProviderPattern({Key key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    final controller = useProvider(sampleStateNotifierProvider);
+    return FloatingActionButton(
+      onPressed: controller.increment,
+    );
+  }
+}
+
+// context.readの場合
+// ボタン押下時ににinit, increment, dispose をすべて行う
+class _FabContextPattern extends StatelessWidget {
+  const _FabContextPattern({Key key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      onPressed: () => context.read(sampleStateNotifierProvider).increment(),
+    );
+  }
+}
