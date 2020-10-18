af57b81616face977f520d5ef44053274a33f299
HTsuruo <tsuru.dev@gmail.com>
2020-10-18 14:19:35 +0900

    ContinuousRectangleBorderをRoundRectangleBorderと比較

---
+++ lib/main_round_rectangle.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const App());
+
+class App extends StatelessWidget {
+  const App({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Container(
+              height: 100,
+              width: MediaQuery.of(context).size.width / 2,
+              child: Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Text('RoundedRectangleBorder'),
+                      Text('R（16）'),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              height: 100,
+              width: MediaQuery.of(context).size.width / 2,
+              child: Card(
+                shape: ContinuousRectangleBorder(
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Text('ContinuousRectangleBorder'),
+                      Text('R（16）'),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              height: 80,
+              width: MediaQuery.of(context).size.width / 2,
+              child: Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(50),
+                ),
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Text('RoundedRectangleBorder'),
+                      Text('R（50）'),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              height: 80,
+              width: MediaQuery.of(context).size.width / 2,
+              child: Card(
+                shape: ContinuousRectangleBorder(
+                  borderRadius: BorderRadius.circular(50),
+                ),
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Text('ContinuousRectangleBorder'),
+                      Text('R（50）'),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
