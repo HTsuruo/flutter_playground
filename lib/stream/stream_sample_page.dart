433d7b5950505763a2f69edbe49a44247c10ae58
HTsuruo <tsuru.dev@gmail.com>
2021-02-25 10:06:47 +0900

    Streamの理解

--- lib/stream/stream_sample_page.dart
+++ lib/stream/stream_sample_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:flutter_playground/stream/rx_sample_controller.dart';
+import 'package:flutter_playground/stream/stream_sample_page_controller.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class StreamSamplePage extends HookWidget {
@@ -11,8 +11,8 @@ class StreamSamplePage extends HookWidget {
   @override
   Widget build(BuildContext context) {
     // 普通のStream/Sinkのサンプル
-    // useProvider(streamSamplePageController);
-    useProvider(rxSampleController);
+    useProvider(streamSamplePageController);
+    // useProvider(rxSampleController);
     return Scaffold(
       appBar: AppBar(
         title: Text(runtimeType.toString()),
--- lib/stream/stream_sample_page_controller.dart
+++ lib/stream/stream_sample_page_controller.dart
@@ -10,11 +10,15 @@ final streamSamplePageController = ChangeNotifierProvider.autoDispose(
 
 class StreamSamplePageController extends ChangeNotifier {
   StreamSamplePageController() {
-    // これは流れてくる
+    //  broadCast指定しない場合、監視前に発行されたイベントも流れてくる
+    // 購読は一つのみのrxdartのreplySubjectと捉えて良さそう？
     _singleController.sink.add('SingleStream0');
+    _singleController.sink.add('SingleStream0.5');
 
     // 購読者1
-    _singleController.stream.listen(logger.fine);
+    _singleController.stream.listen((data) {
+      logger.fine('subscriber[single]:$data');
+    });
 
     // 購読者2（これは通常ブロードキャスト指定をしないと失敗する）
     // Bad state: Stream has already been listened to.
@@ -23,27 +27,50 @@ class StreamSamplePageController extends ChangeNotifier {
     _singleController.sink.add('SingleStream1');
     _singleController.sink.add('SingleStream2');
 
-    // TODO(tsuruoka): Broadcast指定したことによって流れてこなくなった
+    // Broadcast指定したことによって流れてこなくなった
     // BroadCast配信の場合監視前にsink.addしたものは流れてこない
     // 監視する場所とsink.addする箇所が重要で、おそらく監視している状態でないと流せない?
     _broadcastController.sink.add('BroadCast0');
     // 購読者1
     _broadcastController.stream.listen((data) {
-      logger.fine('subscriber1:$data');
+      logger.fine('subscriber[A]:$data');
     });
     // 購読者2
     _broadcastController.stream.listen((data) {
-      logger.fine('subscriber2:$data');
+      logger.fine('subscriber[B]:$data');
     });
     _broadcastController.sink.add('BroadCast1');
     _broadcastController.sink.add('BroadCast2');
 
-    // TODO(tsuruoka): BroadCast1, BroadCast2が流れてこないのが不思議
-    // また、監視後にsink.addすると（BroadCast3）流れてくるがBroadCast1,BroadCast2は消える
+    // transformで監視している購読は`BroadCast3`がかなり早いタイミングで流れてくる
     _broadcastController.stream.transform(_transform()).listen((data) {
-      logger.fine('subscriber3:$data');
+      logger.fine('subscriber[C]:$data');
     });
     _broadcastController.sink.add('BroadCast3');
+
+    // StreamSubscriptionに渡してみる
+    _sh = _broadcastController.stream.listen((data) {
+      logger.fine('subscriber[D]:$data');
+    });
+
+    // StreamSubscriptionはこのような形でstreamデータをハンドリングすることが可能
+    // ちなみに`onData`指定した場合、前述での監視はされる`onData`が優先される
+    // 重複で監視することにはならない.　とはいえonDataであれば普通にlistenすれば良いので使う機会は少なそう
+    // _sh.onData((data) {
+    //   logger.fine('subscriber[D]:onData:$data');
+    // });
+
+    _broadcastController.sink.add('BroadCast4');
+
+    Future(() async {
+      await Future<void>.delayed(const Duration(seconds: 3));
+      _sh.cancel();
+      logger.fine('---subscriber[D] canceled---');
+      // StreamSubscriptionをcancelすることでsubscriber[D]は`BroadCast5`を
+      // 受け取らないというハンドリングができる
+      // cancel以外にも`pause`や`resume`で流し込むstreamを一時停止,再開したりできる
+      _broadcastController.sink.add('BroadCast5');
+    });
   }
 
   // 一人の購読者に向けたStreamのコントローラ
@@ -59,10 +86,21 @@ class StreamSamplePageController extends ChangeNotifier {
         handleData: (value, sink) => sink.add('[HEADER]:$value'));
   }
 
+  // listenの型がStreamSubscriptionで購読の状態をハンドリングすることができる
+  StreamSubscription<String> _sh;
+
   @override
   void dispose() {
     _singleController.close();
     _broadcastController.close();
+
+    // 今回の場合`_broadcastController.close()`でstreamController自体をcloseしているので
+    // わざわざStreamSubscriptionで管理してcloseしなくとも
+    // streamが流れっぱなしでメモリリーク的なことが起こる心配がないが、
+    // 例えば監視する対象が他のファイルで定義されていたりFirestoreのstreamなどの場合は
+    // streamそのもの自体を閉じることはできない?ので、StreamSubscriptionで監視対象を管理し
+    // `cancel()`を呼ぶことでstreamが流れてくるのを塞ぎ止めている理解
+    _sh.cancel();
     super.dispose();
   }
 }
