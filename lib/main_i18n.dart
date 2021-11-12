8f097568adcb27bb1ffbb42828445d130cca2613
tsuruo <tsuru.dev@gmail.com>
2021-11-12 11:45:20 +0900

    Add i18n

---
+++ lib/main_i18n.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+// ref. https://flutter.dev/docs/development/accessibility-and-localization/internationalization
+// ref. https://ch241.lilacwells.com/ja/articles/flutter-l10n/post/
+void main() {
+  runApp(
+    const App(),
+  );
+}
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.from(
+        colorScheme: const ColorScheme.light(),
+      ),
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(Localizations.localeOf(context).languageCode),
+      ),
+      body: Center(
+        child: Text(AppLocalizations.of(context)!.helloWorld),
+      ),
+    );
+  }
+}
