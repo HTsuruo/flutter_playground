806b61a6367ca441dacba14a54a75833d3165762
tsuruo <tsuru.dev@gmail.com>
2022-04-10 11:41:02 +0900

    AnimatedSwithcerを色々触ってみる

---
+++ lib/animation/animated_switcher.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+class AnimatedSwitcherPage extends StatefulWidget {
+  const AnimatedSwitcherPage({Key? key}) : super(key: key);
+
+  static const routeName = '/animated_switcher';
+
+  @override
+  State<AnimatedSwitcherPage> createState() => _AnimatedSwitcherPageState();
+}
+
+class _AnimatedSwitcherPageState extends State<AnimatedSwitcherPage> {
+  var _isDefault = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(AnimatedSwitcherPage.routeName),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            _isDefault = !_isDefault;
+          });
+        },
+        child: const Icon(Icons.refresh),
+      ),
+      body: Center(
+        child: AnimatedSwitcher(
+          duration: const Duration(milliseconds: 500),
+          // reverseDuration: const Duration(seconds: 2),
+
+          // transitionBuilderでTransition系アニメーションをカスタマイズできる
+          // デフォルトはFadeTransition
+          // transitionBuilder: (child, animation) {
+          //   return AlignTransition(
+          //     alignment: animation.drive(
+          //       AlignmentTween(
+          //         begin: Alignment.topLeft,
+          //         end: Alignment.center,
+          //       ),
+          //     ),
+          //     child: child,
+          //   );
+          // },
+
+          // 新旧Widgetの配置もカスタマイズできる
+          // デフォルトはStackかつCenter
+          // layoutBuilder: (child, previousChildren) {
+          //   return Column(
+          //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          //     children: [
+          //       if (child != null) child,
+          //       ...previousChildren,
+          //     ],
+          //   );
+          // },
+          child: _isDefault ? const HogeBox() : const PiyoBox(),
+        ),
+      ),
+    );
+  }
+}
+
+class HogeBox extends StatelessWidget {
+  const HogeBox({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 200,
+      height: 200,
+      color: Colors.blueAccent,
+    );
+  }
+}
+
+class PiyoBox extends StatelessWidget {
+  const PiyoBox({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 300,
+      height: 300,
+      color: Colors.orange,
+    );
+  }
+}
--- lib/animation/main.dart
+++ lib/animation/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_playground/animation/animated_switcher.dart';
+import 'package:flutter_playground/playground_builder.dart';
 
 void main() {
-  runApp(const App());
+  runApp(
+    const App(),
+  );
 }
 
 class App extends StatelessWidget {
@@ -10,113 +13,19 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    const locale = Locale('ja');
+    final routes = <String, WidgetBuilder>{
+      AnimatedSwitcherPage.routeName: (context) => const AnimatedSwitcherPage(),
+    };
     return MaterialApp(
-      locale: locale,
-      supportedLocales: const [locale],
-      localizationsDelegates: const [
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
       theme: ThemeData.from(
-        colorScheme: const ColorScheme.light(),
-      ).copyWith(
-        dividerTheme: const DividerThemeData(space: 0),
-      ),
-      home: const HomePage(),
-    );
-  }
-}
-
-// AnimationControllerを使ってハンドリングするスタンダードなアニメーション方法
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage>
-    with SingleTickerProviderStateMixin {
-  late final AnimationController controller = AnimationController(
-    duration: const Duration(seconds: 3),
-    vsync: this,
-  );
-  late final curvedAnimate = CurvedAnimation(
-    parent: controller,
-    curve: Curves.easeInOut,
-  );
-  late final Animation<Color?> animation =
-      ColorTween(begin: Colors.redAccent, end: Colors.blue)
-          .animate(curvedAnimate);
-
-  @override
-  void initState() {
-    super.initState();
-    animation.addListener(() {
-      setState(() {});
-    });
-    // animation.addStatusListener((status) {
-    //   logger.info('animationStatus: $status');
-    //   switch (status) {
-    //     case AnimationStatus.completed:
-    //       controller.reverse();
-    //       break;
-    //     case AnimationStatus.dismissed:
-    //       controller.forward();
-    //       break;
-    //     case AnimationStatus.reverse:
-    //     case AnimationStatus.forward:
-    //       break;
-    //   }
-    // });
-    controller.forward();
-  }
-
-  @override
-  void dispose() {
-    controller.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          runtimeType.toString(),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color(0xFF2E5817),
         ),
+      ).copyWith(useMaterial3: true),
+      home: PlaygroundBuilder(
+        title: 'Animation',
+        routes: routes,
       ),
-      body: Column(
-        children: [
-          Container(
-            width: 100,
-            height: 100,
-            color: animation.value,
-          ),
-          Text('value: ${animation.value}'),
-          _AnimatedContainer(animation: animation),
-        ],
-      ),
-    );
-  }
-}
-
-// AnimatedWidgetを継承すれば、リスナーの登録やsetStateの再描画などを意識する必要がなくなる
-class _AnimatedContainer extends AnimatedWidget {
-  const _AnimatedContainer({
-    Key? key,
-    required this.animation,
-  }) : super(key: key, listenable: animation);
-
-  final Animation<Color?> animation;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: 100,
-      height: 100,
-      color: animation.value,
     );
   }
 }
