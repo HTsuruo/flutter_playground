a576cde0fbb24174ef2af37c98195ccd2c5caec0
tsuruo <tsuru.dev@gmail.com>
2022-04-24 22:20:17 +0900

    Upgrade packages

--- lib/main_web_view.dart
+++ lib/main_web_view.dart
@@ -16,6 +16,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData.from(
         colorScheme: ColorScheme.fromSeed(
           seedColor: const Color(0xFF2E5817),
