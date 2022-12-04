5f241158846dcc86a9e19780764e431cc9ec466b
tsuruo <tsuru.dev@gmail.com>
2022-12-05 08:16:00 +0900

    icon_font_generatorで生成

---
+++ lib/fruits_icons_2.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/widgets.dart';
+
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: non_constant_identifier_names
+// ignore_for_file: constant_identifier_names
+@immutable
+class _FruitsIconsData extends IconData {
+  const _FruitsIconsData(int codePoint, this.name)
+      : super(
+          codePoint,
+          fontFamily: 'FruitsIcons',
+        );
+
+  final String name;
+}
+
+@immutable
+class FruitsIcons {
+  const FruitsIcons._();
+
+  static const fruits_apple = _FruitsIconsData(0xf101, 'fruits_apple');
+  static const fruits_grape = _FruitsIconsData(0xf102, 'fruits_grape');
+  static const fruits_pear = _FruitsIconsData(0xf103, 'fruits_pear');
+
+  static const all = <String, _FruitsIconsData>{
+    'fruits_apple': fruits_apple,
+    'fruits_grape': fruits_grape,
+    'fruits_pear': fruits_pear,
+  };
+}
